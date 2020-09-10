6a02184be4b9094f895f80da5abd04b00cbdc9ba
Jason Turney <jnt5211@psu.edu>
2020-09-10 16:53:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# Author: Jason Turney jnt5211@psu.edu
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 1
+# Breakout: 12
+
+def getLetterGrade(grade):
+  if grade >= 93.0:
+    print("Your letter grade for CMPSC 131 is A.") 
+  if grade >= 90.0 and grade < 93.0:
+    print("Your letter grade for CMPSC 131 is A-.")
+  if grade >= 87.0 and grade < 90.0:
+    print("Your letter grade for CMPSC 131 is B+.")
+  if grade >= 83.0 and grade < 87.0:
+    print("Your letter grade for CMPSC 131 is B.")
+  if grade >= 80.0 and grade < 83.0:
+    print("Your letter grade for CMPSC 131 is B-.")
+  if grade >= 77.0 and grade < 80.0:
+    print("Your letter grade for CMPSC 131 is C+.")
+  if grade >= 70.0 and grade < 77.0:
+    print("Your letter grade for CMPSC 131 is C.")
+  if grade >= 70.0 and grade < 60.0:
+    print("Your letter grade for CMPSC 131 is D.")
+  if grade < 60:
+    print("Your letter grade for CMPSC 131 is F.")
